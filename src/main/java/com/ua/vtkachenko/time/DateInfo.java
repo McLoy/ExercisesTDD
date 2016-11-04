214b0c11a8440ae62fb2ed9a86acf3bf9f9770c8
McLoy <tkachenko.vlad@hotmail.com>
2016-11-05 01:46:56 +0200

    WorkCalendar: fixing bugs

--- src/main/java/com/ua/vtkachenko/time/DateInfo.java
+++ src/main/java/com/ua/vtkachenko/time/DateInfo.java
@@ -10,13 +10,11 @@ public class DateInfo {
     private String description;
 
     public DateInfo(LocalDate date, DayType type, String description) {
-
         Objects.requireNonNull(date);
         Objects.requireNonNull(type);
         this.date = date;
         this.type = type;
         this.description = description;
-
     }
 
     public LocalDate getDate() {
@@ -33,23 +31,17 @@ public class DateInfo {
 
     @Override
     public boolean equals(Object o) {
-
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         DateInfo dateInfo = (DateInfo) o;
-
         return date != null ? date.equals(dateInfo.date) : dateInfo.date == null;
-
     }
 
     @Override
     public int hashCode() {
-
         if (date != null){
             int c = date.getDayOfYear();
             return 37*c + c;
         } else return 0;
-
     }
 }
